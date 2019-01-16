3acaf581299db1f7650f4d8f5ee950c7b2c7d08b
rumsky <zhaohongyuu@163.com>
2019-01-16 18:50:42 +0800

    wx share test

--- src/index.js
+++ src/index.js
@@ -12,6 +12,7 @@ import { dateFormat } from 'utils';
 import ReactGA from 'react-ga';
 import IntlProviderWrap from './components/IntlProviderWrap';
 import App from './App';
+import axios from 'axios';
 
 import('./styles/main.scss');
 
@@ -65,6 +66,56 @@ const renderApp = Component => {
     </AppContainer>,
     document.getElementById('root')
   );
+
+  axios
+    .get(
+      `http://fe.btc.com/wechat/token?url=http://fork-eth-dev.btc.com&name=ethFork&type=json&debug=true`
+    )
+    .then(res => {
+      let errorInfo = 'not error';
+      debugger;
+      if (res && res.data) {
+        debugger;
+        let data = res.data;
+        errorInfo = JSON.stringify(data);
+        console.log(data);
+        wx.config = {
+          debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert，参数信息会通过log打出。
+          appId: 'wxd5b42c7a0f3817bf', // 必填，公众号的唯一标识
+          timestamp: data.wxToken.timestamp, // 必填，生成签名的时间戳
+          nonceStr: 'pzkcwxklw82elvwarv0fp2y9zau0u2xaggrm0nvq0m', // 必填，生成签名的随机串
+          signature: data.wxToken.signature, // 必填，签名，见附录1
+          jsApiList: [
+            'onMenuShareTimeline',
+            'updateAppMessageShareData',
+            'updateTimelineShareData'
+          ]
+        };
+
+        wx.ready(() => {
+          errorInfo = 'config ready';
+        });
+
+        wx.error(res => {
+          errorInfo = JSON.stringify(res);
+        });
+
+        wx.onMenuShareTimeline({
+          title: '分享测试',
+          link: 'http://fork-eth-dev.btc.com',
+          imgUrl: 'https://eth.btc.com/ec9cb9684dfff6ebdfb496989d224363.png',
+          success: function() {
+            errorInfo = 'onMenuShareTimeline success';
+            // alert(3);
+          },
+          cancel: function() {
+            errorInfo = 'onMenuShareTimeline cancel';
+            // alert(5);
+          }
+        });
+      }
+      document.getElementById('errorInfo').innerText = errorInfo;
+    });
 };
 
 if (isProduction) {
--- src/stores/AppStore.js
+++ src/stores/AppStore.js
@@ -1,5 +1,4 @@
 import { observable, action, runInAction } from 'mobx';
-import axios from 'axios';
 
 class AppStore {
   @observable
@@ -61,72 +60,6 @@ class AppStore {
   @action
   wxSignature = async () => {
     this.errorInfo = 'start wx share';
-    const res = await axios.get(
-      `http://fe.btc.com/wechat/token?url=http://fork-eth-dev.btc.com&name=ethFork&type=json&debug=true`
-    );
-    if (res && res.data) {
-      runInAction(() => {
-        let data = res.data;
-        this.errorInfo = JSON.stringify(data);
-        console.log(data);
-        wx.config = {
-          debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert，参数信息会通过log打出。
-          appId: 'wxd5b42c7a0f3817bf', // 必填，公众号的唯一标识
-          timestamp: data.wxToken.timestamp, // 必填，生成签名的时间戳
-          nonceStr: 'pzkcwxklw82elvwarv0fp2y9zau0u2xaggrm0nvq0m', // 必填，生成签名的随机串
-          signature: data.wxToken.signature, // 必填，签名，见附录1
-          jsApiList: [
-            'onMenuShareTimeline',
-            'updateAppMessageShareData',
-            'updateTimelineShareData'
-          ]
-        };
-
-        wx.ready(() => {
-          this.errorInfo = 'config ready';
-        });
-
-        wx.error(res => {
-          this.errorInfo = JSON.stringify(res);
-        });
-
-        wx.onMenuShareTimeline({
-          title: '分享测试',
-          link: 'http://fork-eth-dev.btc.com',
-          imgUrl: 'https://eth.btc.com/ec9cb9684dfff6ebdfb496989d224363.png',
-          success: function() {
-            this.errorInfo = 'onMenuShareTimeline success';
-            // alert(3);
-          },
-          cancel: function() {
-            this.errorInfo = 'onMenuShareTimeline cancel';
-            // alert(5);
-          }
-        });
-
-        //需在用户可能点击分享按钮前就先调用
-        wx.updateAppMessageShareData({
-          title: '分享测试',
-          desc: '这是微信分享测试',
-          link: 'http://fork-eth-dev.btc.com',
-          imgUrl: 'https://eth.btc.com/ec9cb9684dfff6ebdfb496989d224363.png',
-          success: function() {
-            this.errorInfo = 'updateAppMessageShareData success';
-            // alert(1);
-          }
-        });
-
-        wx.updateTimelineShareData({
-          title: '分享测试2',
-          link: 'http://fork-eth-dev.btc.com',
-          imgUrl: 'https://eth.btc.com/ec9cb9684dfff6ebdfb496989d224363.png',
-          success: function() {
-            this.errorInfo = 'updateTimelineShareData success';
-            // alert(2);
-          }
-        });
-      });
-    }
   };
   //
 }
