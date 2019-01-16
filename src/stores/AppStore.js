3cce92571a4bde62de9bb7ed9cbdf2ae27dbd370
rumsky <zhaohongyuu@163.com>
2019-01-16 18:04:35 +0800

    wx share test

--- src/stores/AppStore.js
+++ src/stores/AppStore.js
@@ -69,10 +69,23 @@ class AppStore {
           timestamp: data.timestamp, // 必填，生成签名的时间戳
           nonceStr: 'pzkcwxklw82elvwarv0fp2y9zau0u2xaggrm0nvq0m', // 必填，生成签名的随机串
           signature: data.signature, // 必填，签名，见附录1
-          jsApiList: ['updateAppMessageShareData', 'updateTimelineShareData']
+          jsApiList: [
+            'onMenuShareTimeline',
+            'updateAppMessageShareData',
+            'updateTimelineShareData'
+          ]
         };
 
         wx.ready(function() {
+          wx.onMenuShareTimeline({
+            title: '分享测试', // 分享标题
+            link: 'http://fork-eth-dev.btc.com', // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+            imgUrl: 'https://eth.btc.com/ec9cb9684dfff6ebdfb496989d224363.png',
+            success: function() {
+              alert(3);
+            }
+          });
+
           //需在用户可能点击分享按钮前就先调用
           wx.updateAppMessageShareData({
             title: '分享测试', // 分享标题
