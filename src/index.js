7d809c02d27ad0e65fb975862d036a6a176bfa9d
rumsky <zhaohongyuu@163.com>
2019-01-17 16:06:47 +0800

    add wx share

--- src/index.js
+++ src/index.js
@@ -12,6 +12,7 @@ import { dateFormat } from 'utils';
 import ReactGA from 'react-ga';
 import IntlProviderWrap from './components/IntlProviderWrap';
 import App from './App';
+import axios from 'axios';
 
 import('./styles/main.scss');
 
@@ -65,6 +66,55 @@ const renderApp = Component => {
     </AppContainer>,
     document.getElementById('root')
   );
+
+  axios
+    .get(
+      `http://fe.btc.com/wechat/token?url=http://fork-eth-dev.btc.com&name=ethFork&type=json&debug=false`
+    )
+    .then(res => {
+      let errorInfo = 'not error';
+      if (res && res.data) {
+        let data = res.data;
+        errorInfo = JSON.stringify(data);
+        console.log(data);
+        wx.config = {
+          debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert，参数信息会通过log打出。
+          appId: 'wxc755b120860aea61', // 必填，公众号的唯一标识
+          timestamp: data.wxToken.timestamp, // 必填，生成签名的时间戳
+          nonceStr: 'undefined', // 必填，生成签名的随机串
+          signature: data.wxToken.signature, // 必填，签名，见附录1
+          jsApiList: [
+            'onMenuShareTimeline',
+            'updateAppMessageShareData',
+            'updateTimelineShareData'
+          ]
+        };
+        console.log(wx.config);
+        wx.ready(() => {
+          errorInfo = 'config ready';
+        });
+
+        wx.error(res => {
+          errorInfo = JSON.stringify(res);
+        });
+
+        wx.onMenuShareTimeline({
+          title: '分享测试',
+          link: 'https://fork-eth.btc.com',
+          imgUrl: 'https://eth.btc.com/ec9cb9684dfff6ebdfb496989d224363.png',
+          success: function() {
+            errorInfo = 'onMenuShareTimeline success';
+            // alert(3);
+          },
+          cancel: function() {
+            errorInfo = 'onMenuShareTimeline cancel';
+            // alert(5);
+          }
+        });
+      }
+      //document.getElementById('errorInfo').innerText = errorInfo;
+    });
+  //
 };
 
 if (isProduction) {
