38e5724a4bbe918c737e21fb19e2c66bd71e526a
rumsky <zhaohongyuu@163.com>
2019-01-18 16:14:21 +0800

    add devtest bundle

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,7 +1,7 @@
 const isProduction = process.env.NODE_ENV === 'production';
 const baseURL = isProduction
   ? `https://explorer-web.api.btc.com/v1/eth/`
-  : `https://explorer-web.api.btc.com/v1/eth/`;
+  : `https://explorer-web-dev.api.btc.com/v1/eth/`;
 
 //const socketIOURL = 'http://54.169.197.219:17002';
 const socketIOURL = 'https://explorer-web.api.btc.com';
