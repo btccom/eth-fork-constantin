a1f3ebe83e70fa6fd803eb42ec91ec6fa1afa9c7
rumsky <hongyu.zhao@bitmain.com>
2019-01-25 10:50:27 +0800

    git test

--- src/index.js
+++ src/index.js
@@ -80,7 +80,7 @@ const renderApp = Component => {
       let errorInfo = '0';
       if (res && res.data) {
         let data = res.data;
-        errorInfo = '999';
+        errorInfo = '99';
         setInfo(errorInfo);
         console.log(data);
         wx.config = {
